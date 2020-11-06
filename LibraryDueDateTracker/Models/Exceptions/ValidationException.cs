374f170665d54344e4adfa8b4c39a88ce7c9bfdd
waiscodes <b.wais@outlook.com>
2020-11-05 20:46:26 -0700

    added validation exception model

---
+++ LibraryDueDateTracker/Models/Exceptions/ValidationException.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace LibraryDueDateTracker.Models.Exceptions
+{
+    public class ValidationException : Exception
+    {
+        public List<Exception> ValidationExceptions { get; set; }
+
+
+        public ValidationException() : base("Something went wrong. See below")
+        {
+            ValidationExceptions = new List<Exception>();
+        }
+
+        public ValidationException(string message) : base("Something went wrong. See below")
+        {
+            ValidationExceptions = new List<Exception>()
+            {
+                new Exception(message)
+            };
+        }
+    }
+}
